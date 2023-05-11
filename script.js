a13eb5719d57030b38380d6aa1261285b0ab094e
Yahikopnj <benjamin.martel@live.fr>
2023-05-11 09:42:52 +0200

    premier flex ( le tout premier exo )

---
+++ script.js
@@ -0,0 +1,18 @@
+function changecouleur(){
+    var elem1 = document.getElementById("enfant1");
+    elem1.style.backgroundColor="yellow";
+    elem1.style.width="250px";
+    elem1.style.height="120px";
+}
+function souriskipasse(){
+    var elem2 = document.getElementById("enfant2");
+    elem2.style.backgroundColor="grey";
+    elem2.style.width="4000px";
+    elem2.style.height="1200px";
+}
+function doubleclick(){
+    var elem3= document.getElementById("grandparent1");
+    elem3.style.backgroundColor="purple";
+    elem3.style.width="1200px";
+    elem3.style.height="1200px";
+}
